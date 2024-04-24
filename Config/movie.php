1696cc027e09fa86d0d95a494180a9d50cba5e07
Namit <senjaliyanamit123@gmail.com>
2024-04-24 17:56:11 +0530

    first commit

---
+++ Config/movie.php
@@ -0,0 +1,17 @@
+<?php
+
+class data{
+
+    private $name = 'root@localhost';
+    private $server = '127.0.0.1';
+    private $serverType = 'MariaDB';
+    private $password = '';
+
+    function checkConnection(){
+
+
+    }
+
+}
+
+?>
